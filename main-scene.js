5ebb6e573543745229817634581ec670b3f0299f
brayan2800 <brayan2800@gmail.com>
2019-02-20 23:25:46 -0800

    Adjusting skybox to look more natural

--- main-scene.js
+++ main-scene.js
@@ -49,19 +49,19 @@ class Assignment_Two_Skeleton extends Scene_Component {
         // Load some textures for the demo shapes
         this.shape_materials = {};
         const shape_textures = {
-            square: "assets/grass.png",
+            ball: "assets/cow2.png",
+            square: "assets/grass2.png",
             square_sky: "assets/hills_up.png",
             square_LF: "assets/hills_lf.png",
             square_RT: "assets/hills_rt.png",
             square_BK: "assets/hills_bk.png",
-            square_FR: "assets/hills_ft.png",
-            box: "assets/even-dice-cubemap.png",
-            ball: "assets/cow2.png",
+            square_FR: "assets/hills_ft.png"
+            /*box: "assets/even-dice-cubemap.png",
             cylinder: "assets/treebark.png",
             pyramid: "assets/tetrahedron-texture2.png",
             simplebox: "assets/tetrahedron-texture2.png",
             cone: "assets/hypnosis.jpg",
-            circle: "assets/hypnosis.jpg"
+            circle: "assets/hypnosis.jpg"*/
         };
         for (let t in shape_textures)
             this.shape_materials[t] = this.texture_base.override({
@@ -94,33 +94,37 @@ class Assignment_Two_Skeleton extends Scene_Component {
         const t = this.t;
 
         
-//         // Draw some demo textured shapes
-//         let spacing = 6;
-//         let m = Mat4.translation(Vec.of(-1 * (spacing / 2) * (this.shape_count - 1), 0, 0));
-//         for (let k in this.shapes) {
-//             this.shapes[k].draw(
-//                 graphics_state,
-//                 m.
-//                 times(Mat4.rotation(t, Vec.of(0, 1, 0))).
-//                 times(Mat4.scale(Vec.of(2, 2, 1))),
-//                 this.shape_materials[k] || this.plastic);
-//             m = m.times(Mat4.translation(Vec.of(spacing, 0, 0)));
-//         }
+        
+        this.draw_skybox(graphics_state);
 
+        //cow 
         let m = Mat4.identity();
-        let m_column = m;
+        this.shapes["ball"].draw(
+                    graphics_state,
+                    m.times(Mat4.translation(Vec.of(0,1,0))).
+                    times(Mat4.scale(Vec.of(1,1,2))),
+                    this.shape_materials["ball"] || this.plastic);
+   }
+
+   draw_skybox(graphics_state)
+   {
+        let m = Mat4.identity();
+        var m_column;
+
         const grass_length = 10;
-        const n_grass = 10;
+        const n_columns = 20;
+        const size = grass_length*n_columns;
 
         
         m = m.times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
             times(Mat4.scale(Vec.of(grass_length, grass_length,1)));
     
-        //floor plane 
-        for(var i = 0; i < n_grass; i++)
+        //floor plane 200x200
+        //Creating floor out of tiles of the grass object for better scale
+        for(var i = 0; i < n_columns; i++)
         {
             m_column = m;
-            for(var j = 0; j < n_grass; j++)
+            for(var j = 0; j < n_columns; j++)
             {
                 this.shapes["square"].draw(
                     graphics_state,
@@ -137,65 +141,43 @@ class Assignment_Two_Skeleton extends Scene_Component {
         m = Mat4.identity();
         this.shapes["square"].draw(
                     graphics_state,
-                    m.times(Mat4.translation(Vec.of(90,150,-90))).
+                    m.times(Mat4.translation(Vec.of(size-grass_length, size + 30,-size +grass_length))).
                     times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
                     times(Mat4.rotation(Math.PI/2, Vec.of(0, 0, 1))).
-                    times(Mat4.scale(Vec.of(100,100, 1))),
+                    times(Mat4.scale(Vec.of(200,200, 1))),
                     this.shape_materials["square_sky"] || this.plastic);
         //sky_Left
         this.shapes["square"].draw(
                     graphics_state,
-                    m.times(Mat4.translation(Vec.of(-10, 50,-90))).
+                    m.times(Mat4.translation(Vec.of(-grass_length, 30, -size+grass_length))).
                     times(Mat4.rotation(Math.PI/2, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(100,100, 1))),
+                    times(Mat4.scale(Vec.of(200,200, 1))),
                     this.shape_materials["square_LF"] || this.plastic);
         
         //sky_right
         this.shapes["square"].draw(
                     graphics_state,
-                    m.times(Mat4.translation(Vec.of(190,50,-90))).
+                    m.times(Mat4.translation(Vec.of(-grass_length + 2*size,30,-size+grass_length))).
                     times(Mat4.rotation(Math.PI*3/2, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(100,100, 1))),
+                    times(Mat4.scale(Vec.of(200,200, 1))),
                     this.shape_materials["square_RT"] || this.plastic);
 
         //sky_back
         this.shapes["square"].draw(
                     graphics_state,
                     m.
-                    times(Mat4.translation(Vec.of(90,50,-190))).
-                    //times(Mat4.rotation(Math.PI, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(100,100, 1))),
+                    times(Mat4.translation(Vec.of(size-grass_length,30, -2*size+ grass_length))).
+                    times(Mat4.scale(Vec.of(200,200, 1))),
                     this.shape_materials["square_BK"] || this.plastic);
          
         //sky_front
         this.shapes["square"].draw(
                     graphics_state,
                     m.
-                    times(Mat4.translation(Vec.of(90,50,10))).
+                    times(Mat4.translation(Vec.of(size - grass_length,30,grass_length))).
                     times(Mat4.rotation(Math.PI, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(100,100, 1))),
+                    times(Mat4.scale(Vec.of(200,200, 1))),
                     this.shape_materials["square_FR"] || this.plastic);
-
-
-//         //draws 40x40 plot of grass
-//         for (var i =0; i<4; i++)
-//         {
-//             this.shapes["square"].draw(
-//                         graphics_state,
-//                         m.
-//                         times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
-//                         times(Mat4.scale(Vec.of(grass_length/2,grass_length/2,10))),
-//                         this.shape_materials["square"] || this.plastic);
-
-//            m = m.times(Mat4.translation(Vec.of(0,0,20)));
-//         }
-
-        m = Mat4.identity();
-        this.shapes["ball"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(0,1,0))).
-                    times(Mat4.scale(Vec.of(1,1,2))),
-                    this.shape_materials["ball"] || this.plastic);
    }
 
 }
