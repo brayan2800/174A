f5b08fbb9f20a2ff16664e11b09b8908afb9053f
brayan2800 <brayan2800@gmail.com>
2019-03-03 22:21:45 -0800

    Simplifying skybox coordinates and adjusting light source

--- main-scene.js
+++ main-scene.js
@@ -16,6 +16,7 @@ class Assignment_Two_Skeleton extends Scene_Component {
                                                           Vec.of(180.0, 40.0, -50.0), // Position of the light
                                                           Vec.of(200.0, 50.0, -220.0), // Location of where it is looking
                                                           Vec.of(0, 1, 0)); // Up Vector;
+
         context.globals.graphics_state.projection_transform = Mat4.perspective(Math.PI/4, r, .1, 1000);
 
         // At the beginning of our program, load one of each of these shape
@@ -106,10 +107,10 @@ class Assignment_Two_Skeleton extends Scene_Component {
             });
         
         this.lights = [new Light(gl, Mat4.look_at(
-                                          Vec.of(250.0, 230.0, -250.0), // Position of the light
-                                          Vec.of(240.0, 30.0, -220.0), // Location of where it is looking
+                                          Vec.of(210, 230.0, -200), // Position of the light
+                                          Vec.of(200, 30.0, -230), // Location of where it is looking
                                           Vec.of(0, 1, 0)), // Up Vector
-                                          Vec.of(250.0, 230.0, -250.0, 1.0), // Position of the light
+                                          Vec.of(210, 230.0, -200, 1), // Position of the light
                                           Color.of(1.0, 1.0, 1.0, 0.7), 70000)];
 
         this.t = 0;
@@ -121,17 +122,84 @@ class Assignment_Two_Skeleton extends Scene_Component {
     renderShadowmap(graphics_state, t) {
         this.lights[0].renderDepthBuffer(graphics_state, () => {
             let m = Mat4.identity();
-            m = m.times(Mat4.translation(Vec.of(220.0, 5.0, -220.0)));  
+            m = m.times(Mat4.translation(Vec.of(200, 10, -200)));  
             this.draw_cow(graphics_state, m, true);
         });
 
         this.lights[0].renderDepthBuffer(graphics_state, () => {
             let m = Mat4.identity();
-            m = m.times(Mat4.translation(Vec.of(/*240*/ 200 + 20*Math.sin(0.2*(this.t)), 30, -220)));
+            m = m.times(Mat4.translation(Vec.of(200 + 20*Math.sin(0.2*(this.t)), 30, -200)));
             this.draw_ufo(graphics_state, m, true);
         });
     }
     
+    draw_skybox(graphics_state)
+   {
+        /* sides: 400 x 400 
+           floor: spans [(0,0,0), (400, 0, -400)] 
+           
+           Sides are shifted down to make floor-side transitions smoother
+           range inside skybox: [(0,0,0), (400, 230, -400)]
+        */
+
+        let m = Mat4.identity().times(Mat4.translation(Vec.of(200, 0 ,-200)));
+        const size = 200; 
+
+        //sky_up
+        //m = Mat4.identity();
+        this.shapes["square"].draw(
+                    graphics_state,
+                    m.times(Mat4.translation(Vec.of(0, size+30 ,0))).
+                    times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
+                    times(Mat4.rotation(Math.PI/2, Vec.of(0, 0, 1))).
+                    times(Mat4.scale(Vec.of(size,size, 1))),
+                    this.shape_materials["square_sky"] || this.plastic);
+
+        //sky_Left
+        this.shapes["square"].draw(
+                     graphics_state,
+                     m.times(Mat4.translation(Vec.of(-size, 30, 0))).
+                     times(Mat4.rotation(Math.PI/2, Vec.of(0, 1, 0))).
+                     times(Mat4.scale(Vec.of(size,size, 1))),
+                     this.shape_materials["square_LF"] || this.plastic);
+
+        //sky_right
+        this.shapes["square"].draw(
+                    graphics_state,
+                    m.times(Mat4.translation(Vec.of(size,30,0))).
+                    times(Mat4.rotation(Math.PI*3/2, Vec.of(0, 1, 0))).
+                    times(Mat4.scale(Vec.of(size,size, 1))),
+                    this.shape_materials["square_RT"] || this.plastic);
+
+        //sky_back
+        this.shapes["square"].draw(
+                    graphics_state,
+                    m.times(Mat4.translation(Vec.of(0,30, -size))).
+                    times(Mat4.scale(Vec.of(size,size, 1))),
+                    this.shape_materials["square_BK"] || this.plastic);
+
+        //sky_front
+        this.shapes["square"].draw(
+                    graphics_state,
+                    m.times(Mat4.translation(Vec.of(0,30,size))).
+                    times(Mat4.rotation(Math.PI, Vec.of(0, 1, 0))).
+                    times(Mat4.scale(Vec.of(size,size, 1))),
+                    this.shape_materials["square_FR"] || this.plastic);
+
+        // testfloor
+        this.renderShadowmap(graphics_state);
+
+        this.lights[0].renderOutputBuffer(graphics_state, () => {
+            this.shapes["square"].draw(
+                    graphics_state,
+                    m.times(Mat4.rotation(-Math.PI/2, Vec.of(1, 0, 0)))
+                    .times(Mat4.scale(Vec.of(size,size, 1))),
+                    this.floor);
+        });
+
+        this.lights[0].clearDepthBuffer();
+   }
+
     draw_cow(graphics_state, m, depth_test) {
         this.shapes["ball"].draw(
                     graphics_state,
@@ -202,113 +270,26 @@ class Assignment_Two_Skeleton extends Scene_Component {
         if (!this.paused)
             this.t += graphics_state.animation_delta_time / 1000;
         const t = this.t;
+        
         let m = Mat4.identity();
 
         //cow
-        m = m.times(Mat4.translation(Vec.of(220.0, 5.0, -220.0)));
+        m = m.times(Mat4.translation(Vec.of(200, 10, -200)));
         this.draw_cow(graphics_state, m, false);
 
         // skybox
         this.draw_skybox(graphics_state);
 
         // ufo
-        m = Mat4.identity();
+        let x_motion = 180 + 20*Math.sin(0.2*(this.t));
+        let y_motion = 30 + 20*Math.sin(0.2*(this.t));
+        let z_motion = -220;
 
-        m = m.times(Mat4.translation(Vec.of(200 + 20*Math.sin(0.2*(this.t)), 30, -220)));
+        m = Mat4.identity();
+        m = m.times(Mat4.translation(Vec.of(x_motion, 30, -200)));
         this.draw_ufo(graphics_state, m, false);
+
    }
-
-   draw_skybox(graphics_state)
-   {
-        let m = Mat4.identity();
-        var m_column;
-
-        const grass_length = 10;
-        const n_columns = 20;
-        const size = grass_length*n_columns;
-
-        
-        m = m.times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
-            times(Mat4.scale(Vec.of(grass_length, grass_length,1)));
-        
-        // Currently going for one big square instead of tiling
-        //floor plane 200x200
-        //Creating floor out of tiles of the grass object for better scale
-//         for(var i = 0; i < n_columns; i++)
-//         {
-//             m_column = m;
-//             for(var j = 0; j < n_columns; j++)
-//             {
-//                 this.shapes["square"].draw(
-//                     graphics_state,
-//                     m_column,
-//                     this.shape_materials["square"] || this.plastic);
-
-//                 m_column = m_column.times(Mat4.translation(Vec.of(0,-2,0)))
-//             }
-            
-//             m = m.times(Mat4.translation(Vec.of(2,0,0)));
-//         }
-
-        //sky_up
-        m = Mat4.identity();
-        this.shapes["square"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(size-grass_length, size + 30,-size +grass_length))).
-                    times(Mat4.rotation(Math.PI/2, Vec.of(1, 0, 0))).
-                    times(Mat4.rotation(Math.PI/2, Vec.of(0, 0, 1))).
-                    times(Mat4.scale(Vec.of(200,200, 1))),
-                    this.shape_materials["square_sky"] || this.plastic);
-
-        //sky_Left
-        this.shapes["square"].draw(
-                     graphics_state,
-                     m.times(Mat4.translation(Vec.of(-grass_length, 30, -size+grass_length))).
-                     times(Mat4.rotation(Math.PI/2, Vec.of(0, 1, 0))).
-                     times(Mat4.scale(Vec.of(200,200, 1))),
-                     this.shape_materials["square_LF"] || this.plastic);
-
-        //sky_right
-        this.shapes["square"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(-grass_length + 2*size,30,-size+grass_length))).
-                    times(Mat4.rotation(Math.PI*3/2, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(200,200, 1))),
-                    this.shape_materials["square_RT"] || this.plastic);
-
-        //sky_back
-        this.shapes["square"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(size-grass_length,30, -2*size + grass_length))).
-                    times(Mat4.scale(Vec.of(200,200, 1))),
-                    this.shape_materials["square_BK"] || this.plastic);
-
-        //sky_front
-        this.shapes["square"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(size - grass_length,30,grass_length))).
-                    times(Mat4.rotation(Math.PI, Vec.of(0, 1, 0))).
-                    times(Mat4.scale(Vec.of(200,200, 1))),
-                    this.shape_materials["square_FR"] || this.plastic);
-
-        // testfloor
-        this.renderShadowmap(graphics_state);
-        
-        m = Mat4.identity();
-
-        this.lights[0].renderOutputBuffer(graphics_state, () => {
-            this.shapes["square"].draw(
-                    graphics_state,
-                    m.times(Mat4.translation(Vec.of(size - grass_length, 0, -size + grass_length)))
-                    .times(Mat4.rotation(-Math.PI/2, Vec.of(1, 0, 0)))
-                    //.times(Mat4.rotation(Math.PI/2, Vec.of(0, 0, 1)))
-                    .times(Mat4.scale(Vec.of(200,200, 1))),
-                    this.floor);
-        });
-
-        this.lights[0].clearDepthBuffer();
-   }
-
 }
 
 window.Assignment_Two_Skeleton = window.classes.Assignment_Two_Skeleton = Assignment_Two_Skeleton;
